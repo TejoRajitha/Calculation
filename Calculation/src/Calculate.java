00a64b12fce7a12805a278fb99e452576c4a9ead
Rajitha10 <tejorajitha@gmail.com>
2018-04-15 13:40:21 +0530

    Added two methods

---
+++ Calculation/src/Calculate.java
@@ -0,0 +1,23 @@
+
+public class Calculate 
+{
+	private int a=4;
+	private int b=2;
+	private int c=0;
+	
+	public void add()
+	{
+		c=a+b;
+		System.out.println("Add of two numbers:"+c);
+		
+	}
+	
+	public void sum()
+	{
+		c=a-b;
+		System.out.println("sub of two numbers is:"+c);
+		
+	}
+	
+
+}
---
+++ Calculation/src/CalculateTest.java
@@ -0,0 +1,13 @@
+
+public class CalculateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Calculate c=new Calculate();
+		c.add();
+		c.sum();
+
+	}
+
+}
